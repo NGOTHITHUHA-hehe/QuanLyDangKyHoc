d9620d232c5b4a5ab51c00de3072fb1b9aff851e
NGOTHITHUHA-hehe <a53333@st.thanglong.edu.vn>
2026-06-03 23:17:18 +0700

    Add files via upload

---
+++ sinhvien.h
@@ -0,0 +1,22 @@
+#ifndef SINHVIEN_H
+#define SINHVIEN_H
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class SinhVien{
+    private:
+    string maSV;
+    string hoten;
+    int tuoi;
+
+    public:
+    SinhVien();
+    SinhVien(string, string, int);
+
+    void 
+    
+};
+
+#endif
